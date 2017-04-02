49d1b102c587d9e59026448e8536a9f71b2e4917
Jean-Philippe Bruyère <jp_bruyere@hotmail.com>
2017-04-02 13:58:42 +0200

    basic scrolling text editor

---
+++ src/CrowEditExtentions.cs
@@ -0,0 +1,41 @@
+﻿//
+//  CrowEditExtentions.cs
+//
+//  Author:
+//       Jean-Philippe Bruyère <jp.bruyere@hotmail.com>
+//
+//  Copyright (c) 2017 jp
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using System.Collections.Generic;
+
+namespace CrowEdit
+{
+	public static class CrowEditExtentions
+	{
+		public static List<int> AllIndexesOf(this string str, string value) {
+			if (String.IsNullOrEmpty(value))
+				throw new ArgumentException("the string to find may not be empty", "value");
+			List<int> indexes = new List<int>();
+			for (int index = 0;; index += value.Length) {
+				index = str.IndexOf(value, index);
+				if (index == -1)
+					return indexes;
+				indexes.Add(index);
+			}
+		}
+	}
+}
+
